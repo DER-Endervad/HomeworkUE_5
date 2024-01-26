457d10b61b1e4ad86410dca95ae64547854214f9
Даниил <derendervad@gmail.com>
2024-01-26 22:40:11 +0300

    initial commit

---
+++ Source/HomeworkUE_5/Public/Player/LMADefaultCharacter.h
@@ -0,0 +1,55 @@
+// LeaveMeAlone Game by Netologiya. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "LMADefaultCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class HOMEWORKUE_5_API ALMADefaultCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ALMADefaultCharacter();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY()
+	UDecalComponent* CurrentCursor = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cursor")
+	UMaterialInterface* CursorMaterial = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cursor")
+	FVector CursorSize = FVector(20.0f, 40.0f, 40.0f);
+
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	float YRotation = -20.0f;
+
+	float ArmLength = 500.0f;
+	float ArmMin = 300.0f;
+	float ArmMax = 1000.0f;
+
+	float FOV = 60.0f;
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void MoveCamera(float Value);
+};
